342f6dc7f29ab3f5134062388cb4ebb3ca5d111f
Arghadip Chakraborty <chakrabortyarghadip5@gmail.com>
2019-02-05 13:52:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,12 @@ $(document).ready(function(){
 	$("#resume").click(function(){
 		alert("Temporarily unavailable :(");
 	});
+	$("#blur").mouseenter(function(){
+		$("#blur").css("filter","blur(0px)");
+	});
+	$("#blur").mouseleave(function(){
+	$("#blur").css("filter","blur(1px)");
+	});
+	
 	
 });
