4e31255cb57453314874ef883c7a02be07e264e9
arghac14 <chakrabortyarghadip5@gmail.com>
2019-07-28 00:03:22 +0530

    added more skills

--- public/script.js
+++ public/script.js
@@ -2,5 +2,17 @@ $(document).ready(function(){
 	$("#resume").click(function(){
 		alert("Temporarily unavailable :(");
 	});
+	$("#blur").mouseenter(function(){
+		$("#blur").css("filter","blur(0px)");
+	});
+	$("#blur").mouseleave(function(){
+	$("#blur").css("filter","blur(1px)");
+	});
+	$("#skillbutton").click(function(){
+		$("#skillmeter").slideToggle("slow");
+	
+	  });
+
+	
 	
 });
